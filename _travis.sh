f748daa1a31ed615d2cfb75be6835f920835b372
jliu <liujianglc@163.com>
2018-09-14 22:48:56 +0800

    ci

--- _travis.sh
+++ _travis.sh
@@ -52,14 +52,14 @@ EOF
   # Github Pages
   git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master
   # Gitee Pages
-  git push --force --quiet "https://liujianglc:${GITEE_TOKEN}@${GITEE_REF}" master:master
+  # git push --force --quiet "https://liujianglc:${GITEE_TOKEN}@${GITEE_REF}" master:master
 
   # Create Tag
   git tag v1.2.$TRAVIS_BUILD_NUMBER -a -m "Auto Taged By TravisCI With Build $TRAVIS_BUILD_NUMBER"
   # Github Pages
   git push --quiet "https://${GH_TOKEN}@${GH_REF}" master:master --tags
   # Gitee Pages
-  git push --quiet "https://liujianglc:${GITEE_TOKEN}@${GITEE_REF}" master:master --tags
+  # git push --quiet "https://liujianglc:${GITEE_TOKEN}@${GITEE_REF}" master:master --tags
 }
 
 case $1 in
